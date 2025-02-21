27ca6443436089497e406ea4770961f5c0108d6d
Pikin <pikin@bk.ru>
2025-02-21 16:41:57 +0500

    commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,112 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Server {
+
+    private static final Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        int port = ConsoleHelper.readInt();
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Чат сервер запущен.");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера.");
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        try {
+            for (Connection connection : connectionMap.values()) {
+                connection.send(message);
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Сообщение не отправлено");
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            ConsoleHelper.writeMessage("Установлено новое соединение с адресом " + socket.getRemoteSocketAddress());
+            String userName = null;
+            try(Connection connection = new Connection(socket)) {
+                userName = serverHandshake(connection);
+                sendBroadcastMessage(new Message(MessageType.USER_ADDED, userName));
+                notifyUsers(connection, userName);
+                serverMainLoop(connection, userName);
+            } catch (IOException | ClassNotFoundException e) {
+                ConsoleHelper.writeMessage("Ошибка при обмене данными с " + socket.getRemoteSocketAddress());
+            }
+            if (userName != null) {
+                connectionMap.remove(userName);
+                sendBroadcastMessage(new Message(MessageType.USER_REMOVED, userName));
+            }
+            ConsoleHelper.writeMessage("Соединение с " + socket.getRemoteSocketAddress() + " закрыто.");
+        }
+
+        private void notifyUsers(Connection connection, String userName) throws IOException {
+            for (String name : connectionMap.keySet()) {
+                if (!name.equals(userName)) {
+                    connection.send(new Message(MessageType.USER_ADDED, name));
+                }
+            }
+        }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.TEXT) {
+                    String newMessage = userName + ": " + message.getData();
+                    sendBroadcastMessage(new Message(MessageType.TEXT, newMessage));
+                } else {
+                    ConsoleHelper.writeMessage("Невалидное сообщения");
+                }
+            }
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                Message message = connection.receive();
+                if (message.getType() != MessageType.USER_NAME) {
+                    ConsoleHelper.writeMessage("Получено сообщение от " + socket.getRemoteSocketAddress()
+                            + ". Тип сообщения не соответствует протоколу");
+                    continue;
+                }
+                String userName = message.getData();
+
+                if (userName.isEmpty()) {
+                    ConsoleHelper.writeMessage("Попытка подключения к серверу с пустым именем от "
+                            + socket.getRemoteSocketAddress());
+                    continue;
+                }
+
+                if (connectionMap.containsKey(userName)) {
+                    ConsoleHelper.writeMessage("Попытка подключения к серверу с уже используемым именем от "
+                            + socket.getRemoteSocketAddress());
+                    continue;
+                }
+                connectionMap.put(userName, connection);
+
+                connection.send(new Message(MessageType.NAME_ACCEPTED));
+                return userName;
+            }
+        }
+    }
+}
+
